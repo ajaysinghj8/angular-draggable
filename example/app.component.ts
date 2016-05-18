603e1eff8e3c7c7557a1f4ea52bf4f78ec2e0198
Ajay Singh <meajaysingh@hotmail.com>
2016-05-19 01:51:23 +0530

    multiple draggables

--- example/app.component.ts
+++ example/app.component.ts
@@ -2,7 +2,13 @@ import {Component} from '@angular/core';
 import {Draggable} from '../lib';
 @Component({
     selector: 'my-app',
-    template: '<div [draggable] style="position:fixed;"> <h1> Ajay Singh </h1> </div>',
+    template: `<div [draggable] style="position:fixed;"> <h1> Ajay Singh </h1> </div>
+    <div [draggable] style="position:fixed;"> <h1> is </h1> </div>
+    <div [draggable] style="position:fixed;"> <h1> a </h1> </div>
+    <div [draggable] style="position:fixed;"> <h1> JavaScript </h1> </div>
+    <div [draggable] style="position:fixed;"> <h1> developer</h1> </div>
+    <div [draggable] style="position:fixed;"> <h1> . </h1> </div>
+    <div [draggable] style="position:fixed;"> <h1> Full Stack </h1> </div>`,
     directives: [Draggable] 
 })
 export class AppComponent { }
